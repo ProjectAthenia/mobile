2fb5a1926f3d8461508931391d2c84b8b83cbe58
Bryce Meyer <bryce13950@gmail.com>
2020-07-23 17:48:19 +0200

    added organization management ui

--- src/app/components/organization-users-management/organization-users-management.component.ts
+++ src/app/components/organization-users-management/organization-users-management.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { NavController, Platform } from '@ionic/angular';
+import { Component, Input } from '@angular/core';
 import { RequestsProvider } from '../../providers/requests/requests';
 import {Organization} from '../../models/organization/organization';
+import {OrganizationManager} from '../../models/organization/organization-manager';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-organization-users-management',
@@ -16,7 +17,59 @@ export class OrganizationUsersManagementComponent {
     /**
      * Default Constructor
      * @param requests
+     * @param alertController
      */
-    constructor(private requests: RequestsProvider)
-    {}
+    constructor(private requests: RequestsProvider,
+                private alertController: AlertController) {
+    }
+
+    /**
+     * Makes sure the user wants to delete the organization manager
+     * @param organizationManager
+     */
+    promptDeletion(organizationManager: OrganizationManager) {
+        let alert;
+        this.alertController.create({
+            header: 'Are you sure?',
+            message: 'This user will no longer be able to access any parts of your organization.',
+            buttons: [
+                {
+                    text: 'No',
+                },
+                {
+                    text: 'Yes',
+                    handler: () => {
+                        alert.dismiss();
+                    }
+                }
+            ]
+        }).then(a => {
+            alert = a;
+            alert.present();
+        });
+    }
+
+    /**
+     * Opens the add member prompt
+     */
+    addMember() {
+        this.alertController.create({
+            header: 'Add Member',
+            message: 'Enter the email address of the user that you want to add to your organization.',
+            inputs: [
+                {
+                    type: 'email',
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Submit',
+                    handler: (value) => {
+                        console.log(value);
+
+                    }
+                }
+            ],
+        })
+    }
 }
