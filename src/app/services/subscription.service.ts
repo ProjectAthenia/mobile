d9fcc873af60cdb6bf02381f1c9228a677b589ae
Bryce Meyer <bryce13950@gmail.com>
2020-11-30 16:05:48 +0100

    added function to get a feature based on its id to the feature service

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {RequestsProvider} from '../providers/requests/requests';
 import {User} from '../models/user/user';
+import {Feature} from '../models/feature';
 
 @Injectable({
     providedIn: 'root'
@@ -33,5 +34,35 @@ export class SubscriptionService
                 defaultMembershipPlan ? defaultMembershipPlan.containsFeatureId(featureId) : false
             );
         })
+    }
+
+    /**
+     * Gets a feature based on the passed in id if it can be found
+     * rejects the promise if we could not find the feature
+     * @param featureId
+     */
+    public getFeature(featureId: number): Promise<Feature>
+    {
+        // Membership plans are more likely to be used
+        return this.requests.subscriptions.fetchMembershipPlans().then(membershipPlans => {
+            const maybeMembershipPlanWithFeature =
+                membershipPlans.find(membershipPlan => membershipPlan.containsFeatureId(featureId));
+            let maybeFeature = maybeMembershipPlanWithFeature ?
+                maybeMembershipPlanWithFeature.features.find(feature => feature.id == featureId) : null;
+
+            if (maybeFeature) {
+                return Promise.resolve(maybeFeature);
+            } else {
+                // Now we check the features endpoint for the data directly
+                return this.requests.features.fetchFeatures().then(features => {
+                    maybeFeature = features.find(feature => feature.id == featureId);
+                    if (maybeFeature) {
+                        return Promise.resolve(maybeFeature);
+                    }
+
+                    return Promise.reject('Feature not found');
+                });
+            }
+        });
     }
 }
